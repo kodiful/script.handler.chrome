ba5ecb9cf6b9cbe16afc137243cd64324ca0616f
kodiful <uchiyama@mac.com>
2018-03-19 21:34:17 +0900

    basic function works

--- default.py
+++ default.py
@@ -1,22 +1,35 @@
 # -*- coding: utf-8 -*-
 
 import urlparse, urllib
-import sys, re
+import sys, os
 import xbmc, xbmcgui, xbmcplugin, xbmcaddon
 
 from resources.lib.common import log, notify
 
+# import selenium
+addon = xbmcaddon.Addon()
+addon_path = xbmc.translatePath(addon.getAddonInfo('path'))
+sys.path.append(os.path.join(addon_path, 'resources', 'lib'))
+from selenium import webdriver
+
 #-------------------------------------------------------------------------------
 def main():
     # アドオン設定を確認
-    addon = xbmcaddon.Addon()
     browser = addon.getSetting('browser')
+    driver = None
     if browser == 'Chrome':
-        driver = addon.getSetting('chrome')
+        path = addon.getSetting('chrome')
+        if path:
+            driver = webdriver.Chrome(path)
     elif browser == 'Firefox':
-        driver = addon.getSetting('firefox')
-    else:
-        driver = None
+        path = addon.getSetting('firefox')
+        if path:
+            driver = webdriver.Firefox(path)
+    elif browser == 'Safari':
+        path = addon.getSetting('safari')
+        if path:
+            driver = webdriver.Safari(path)
+
     if browser and driver:
         pass
     else:
@@ -28,7 +41,11 @@ def main():
     action = args.get('action', None)
 
     if action is None:
-        pass
+        driver.get('https://www.yahoo.co.jp/')
+        #text = driver.find_element_by_id("srchAssistTxt").text
+        text = driver.title
+        log(text)
+        notify(text)
     elif action[0] == 'settings':
         xbmc.executebuiltin('Addon.OpenSettings(%s)' % addon.getAddonInfo('id'))
 
